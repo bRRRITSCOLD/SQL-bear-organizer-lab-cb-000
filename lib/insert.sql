641476f892d9baafbaaf8cc9d8739a3652ffc382
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-30 00:13:28 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 115, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 13, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 9, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 24, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 3, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 7, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 20, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 11, "M", "black", "aggressive", 0);
